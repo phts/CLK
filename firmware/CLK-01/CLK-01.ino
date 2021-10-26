d68f71c56493bd29c47e1ef41f8928b9b755859c
Phil Tsaryk <phil@tsaryk.com>
2021-10-26 15:20:27 +0200

    refactor: clean up settings and version

--- firmware/CLK-01/CLK-01.ino
+++ firmware/CLK-01/CLK-01.ino
@@ -1,4 +1,11 @@
-#include "version.h"
 #include "app/default_settings.h"
+
+#if __has_include("version.h")
+#include "version.h"
+#endif
+
+#if __has_include("settings.h")
 #include "settings.h"
+#endif
+
 #include "app/app.h"
