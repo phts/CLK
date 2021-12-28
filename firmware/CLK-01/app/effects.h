e274542a95ed279eed01f089dad00b00ca6a4ae6
Phil Tsaryk <phil@tsaryk.com>
2021-12-28 21:36:39 +0100

    refactor(effects): separate functions

--- firmware/CLK-01/app/effects.h
+++ firmware/CLK-01/app/effects.h
@@ -49,311 +49,22 @@ public:
 
   boolean tick(byte hrs, byte mins, byte newTime[])
   {
-    if (mode == EFFECT_NONE)
+    switch (mode)
     {
-      indicators.showTime(hrs, mins);
-      return false;
+    case EFFECT_NONE:
+      return tickEffectNone(hrs, mins, newTime);
+    case EFFECT_DECAY:
+      return tickEffectDecay(hrs, mins, newTime);
+    case EFFECT_LOOP_D:
+      return tickEffectLoopDigits(hrs, mins, newTime);
+    case EFFECT_LOOP_C:
+      return tickEffectLoopCathodes(hrs, mins, newTime);
+    case EFFECT_TRAIN:
+      return tickEffectTrain(hrs, mins, newTime);
+    case EFFECT_RUBBER:
+      return tickEffectRubber(hrs, mins, newTime);
     }
-
-    else if (mode == EFFECT_DECAY)
-    {
-      if (!flipInProgress)
-      {
-        flipInProgress = true;
-        for (byte i = 0; i < INDICATORS_AMOUNT; i++)
-        {
-          indicatorsToFlip[i] = indicators.digits[i] != newTime[i];
-        }
-        decayIndicatorBrightness = indicators.getMaxBrightness();
-      }
-      if (!flipTimer.isReady())
-      {
-        return true;
-      }
-      if (!decayDirection)
-      {
-        decayIndicatorBrightness--;
-        if (decayIndicatorBrightness <= 0)
-        {
-          decayDirection = true;
-          decayIndicatorBrightness = 0;
-          indicators.showTime(hrs, mins);
-        }
-      }
-      else
-      {
-        decayIndicatorBrightness++;
-        if (decayIndicatorBrightness >= indicators.getMaxBrightness())
-        {
-          decayDirection = false;
-          decayIndicatorBrightness = indicators.getMaxBrightness();
-          flipInProgress = false;
-          return false;
-        }
-      }
-      for (byte i = 0; i < INDICATORS_AMOUNT; i++)
-      {
-        if (indicatorsToFlip[i])
-        {
-          indicators.brightness[i] = decayIndicatorBrightness;
-        }
-      }
-    }
-
-    else if (mode == EFFECT_LOOP_D)
-    {
-      if (!flipInProgress)
-      {
-        flipInProgress = true;
-        for (byte i = 0; i < INDICATORS_AMOUNT; i++)
-        {
-          indicatorsToFlip[i] = indicators.digits[i] != newTime[i];
-        }
-      }
-      if (!flipTimer.isReady())
-      {
-        return true;
-      }
-      byte flipCounter = 0;
-      for (byte i = 0; i < INDICATORS_AMOUNT; i++)
-      {
-        if (indicatorsToFlip[i])
-        {
-          indicators.digits[i]--;
-          if (indicators.digits[i] < 0)
-          {
-            indicators.digits[i] = 9;
-          }
-          if (indicators.digits[i] == newTime[i])
-          {
-            indicatorsToFlip[i] = false;
-          }
-        }
-        else
-        {
-          flipCounter++;
-        }
-      }
-      if (flipCounter == INDICATORS_AMOUNT)
-      {
-        flipInProgress = false;
-        return false;
-      }
-    }
-
-    else if (mode == EFFECT_LOOP_C)
-    {
-      if (!flipInProgress)
-      {
-        flipInProgress = true;
-        for (byte i = 0; i < INDICATORS_AMOUNT; i++)
-        {
-          indicatorsToFlip[i] = indicators.digits[i] != newTime[i];
-          if (indicatorsToFlip[i])
-          {
-            for (byte c = 0; c < 10; c++)
-            {
-              if (indicators.digits[i] == CATHOD_TO_DIGIT[c])
-              {
-                startCathode[i] = c;
-              }
-              if (newTime[i] == CATHOD_TO_DIGIT[c])
-              {
-                endCathode[i] = c;
-              }
-            }
-          }
-        }
-      }
-      if (!flipTimer.isReady())
-      {
-        return true;
-      }
-      byte flipCounter = 0;
-      for (byte i = 0; i < INDICATORS_AMOUNT; i++)
-      {
-        if (indicatorsToFlip[i])
-        {
-          if (startCathode[i] > endCathode[i])
-          {
-            startCathode[i]--;
-            indicators.digits[i] = CATHOD_TO_DIGIT[startCathode[i]];
-          }
-          else if (startCathode[i] < endCathode[i])
-          {
-            startCathode[i]++;
-            indicators.digits[i] = CATHOD_TO_DIGIT[startCathode[i]];
-          }
-          else
-          {
-            indicatorsToFlip[i] = false;
-          }
-        }
-        else
-        {
-          flipCounter++;
-        }
-      }
-      if (flipCounter == INDICATORS_AMOUNT)
-      {
-        flipInProgress = false;
-        return false;
-      }
-    }
-
-    else if (mode == EFFECT_TRAIN)
-    {
-      if (!flipInProgress)
-      {
-        flipInProgress = true;
-        currentLamp = 0;
-        trainLeaving = true;
-        flipTimer.reset();
-      }
-      if (!flipTimer.isReady())
-      {
-        return true;
-      }
-      if (trainLeaving)
-      {
-        for (byte i = 3; i > currentLamp; i--)
-        {
-          indicators.digits[i] = indicators.digits[i - 1];
-        }
-        indicators.turnOff(currentLamp);
-        currentLamp++;
-        if (currentLamp >= INDICATORS_AMOUNT)
-        {
-          trainLeaving = false;
-          currentLamp = 0;
-        }
-      }
-      else
-      {
-        for (byte i = currentLamp; i > 0; i--)
-        {
-          indicators.digits[i] = indicators.digits[i - 1];
-        }
-        indicators.digits[0] = newTime[3 - currentLamp];
-        indicators.turnOn(currentLamp);
-        currentLamp++;
-        if (currentLamp >= INDICATORS_AMOUNT)
-        {
-          flipInProgress = false;
-          return false;
-        }
-      }
-    }
-
-    else if (mode == EFFECT_RUBBER)
-    {
-      if (!flipInProgress)
-      {
-        flipInProgress = true;
-        flipEffectStages = 0;
-        flipTimer.reset();
-      }
-      if (!flipTimer.isReady())
-      {
-        return true;
-      }
-      switch (flipEffectStages++)
-      {
-      case 1:
-        indicators.turnOff(3);
-        break;
-      case 2:
-        indicators.turnOff(2);
-        indicators.digits[3] = indicators.digits[2];
-        indicators.turnOn(3);
-        break;
-      case 3:
-        indicators.turnOff(3);
-        break;
-      case 4:
-        indicators.turnOff(1);
-        indicators.digits[2] = indicators.digits[1];
-        indicators.turnOn(2);
-        break;
-      case 5:
-        indicators.turnOff(2);
-        indicators.digits[3] = indicators.digits[1];
-        indicators.turnOn(3);
-        break;
-      case 6:
-        indicators.turnOff(3);
-        break;
-      case 7:
-        indicators.turnOff(0);
-        indicators.digits[1] = indicators.digits[0];
-        indicators.turnOn(1);
-        break;
-      case 8:
-        indicators.turnOff(1);
-        indicators.digits[2] = indicators.digits[0];
-        indicators.turnOn(2);
-        break;
-      case 9:
-        indicators.turnOff(2);
-        indicators.digits[3] = indicators.digits[0];
-        indicators.turnOn(3);
-        break;
-      case 10:
-        indicators.turnOff(3);
-        break;
-      case 11:
-        indicators.digits[0] = newTime[3];
-        indicators.turnOn(0);
-        break;
-      case 12:
-        indicators.turnOff(0);
-        indicators.digits[1] = newTime[3];
-        indicators.turnOn(1);
-        break;
-      case 13:
-        indicators.turnOff(1);
-        indicators.digits[2] = newTime[3];
-        indicators.turnOn(2);
-        break;
-      case 14:
-        indicators.turnOff(2);
-        indicators.digits[3] = newTime[3];
-        indicators.turnOn(3);
-        break;
-      case 15:
-        indicators.digits[0] = newTime[2];
-        indicators.turnOn(0);
-        break;
-      case 16:
-        indicators.turnOff(0);
-        indicators.digits[1] = newTime[2];
-        indicators.turnOn(1);
-        break;
-      case 17:
-        indicators.turnOff(1);
-        indicators.digits[2] = newTime[2];
-        indicators.turnOn(2);
-        break;
-      case 18:
-        indicators.digits[0] = newTime[1];
-        indicators.turnOn(0);
-        break;
-      case 19:
-        indicators.turnOff(0);
-        indicators.digits[1] = newTime[1];
-        indicators.turnOn(1);
-        break;
-      case 20:
-        indicators.digits[0] = newTime[0];
-        indicators.turnOn(0);
-        break;
-      case 21:
-        flipInProgress = false;
-        return false;
-      }
-    }
-
-    return true;
+    return false;
   }
 
 private:
@@ -368,6 +79,315 @@ private:
   boolean decayDirection;
   int decayIndicatorBrightness;
   timerMinim flipTimer;
+
+  bool tickEffectNone(byte hrs, byte mins, byte newTime[])
+  {
+    indicators.showTime(hrs, mins);
+    return false;
+  }
+
+  bool tickEffectDecay(byte hrs, byte mins, byte newTime[])
+  {
+    if (!flipInProgress)
+    {
+      flipInProgress = true;
+      for (byte i = 0; i < INDICATORS_AMOUNT; i++)
+      {
+        indicatorsToFlip[i] = indicators.digits[i] != newTime[i];
+      }
+      decayIndicatorBrightness = indicators.getMaxBrightness();
+      decayDirection = true;
+    }
+    if (!flipTimer.isReady())
+    {
+      return true;
+    }
+    if (decayDirection)
+    {
+      decayIndicatorBrightness--;
+      if (decayIndicatorBrightness <= 0)
+      {
+        decayDirection = false;
+        decayIndicatorBrightness = 0;
+        indicators.showTime(hrs, mins);
+      }
+    }
+    else
+    {
+      decayIndicatorBrightness++;
+      if (decayIndicatorBrightness >= indicators.getMaxBrightness())
+      {
+        decayIndicatorBrightness = indicators.getMaxBrightness();
+        flipInProgress = false;
+        return false;
+      }
+    }
+    for (byte i = 0; i < INDICATORS_AMOUNT; i++)
+    {
+      if (indicatorsToFlip[i])
+      {
+        indicators.brightness[i] = decayIndicatorBrightness;
+      }
+    }
+    return true;
+  }
+
+  bool tickEffectLoopDigits(byte hrs, byte mins, byte newTime[])
+  {
+    if (!flipInProgress)
+    {
+      flipInProgress = true;
+      for (byte i = 0; i < INDICATORS_AMOUNT; i++)
+      {
+        indicatorsToFlip[i] = indicators.digits[i] != newTime[i];
+      }
+    }
+    if (!flipTimer.isReady())
+    {
+      return true;
+    }
+    byte flipCounter = 0;
+    for (byte i = 0; i < INDICATORS_AMOUNT; i++)
+    {
+      if (indicatorsToFlip[i])
+      {
+        indicators.digits[i]--;
+        if (indicators.digits[i] < 0)
+        {
+          indicators.digits[i] = 9;
+        }
+        if (indicators.digits[i] == newTime[i])
+        {
+          indicatorsToFlip[i] = false;
+        }
+      }
+      else
+      {
+        flipCounter++;
+      }
+    }
+    if (flipCounter == INDICATORS_AMOUNT)
+    {
+      flipInProgress = false;
+      return false;
+    }
+    return true;
+  }
+
+  bool tickEffectLoopCathodes(byte hrs, byte mins, byte newTime[])
+  {
+    if (!flipInProgress)
+    {
+      flipInProgress = true;
+      for (byte i = 0; i < INDICATORS_AMOUNT; i++)
+      {
+        indicatorsToFlip[i] = indicators.digits[i] != newTime[i];
+        if (indicatorsToFlip[i])
+        {
+          for (byte c = 0; c < 10; c++)
+          {
+            if (indicators.digits[i] == CATHOD_TO_DIGIT[c])
+            {
+              startCathode[i] = c;
+            }
+            if (newTime[i] == CATHOD_TO_DIGIT[c])
+            {
+              endCathode[i] = c;
+            }
+          }
+        }
+      }
+    }
+    if (!flipTimer.isReady())
+    {
+      return true;
+    }
+    byte flipCounter = 0;
+    for (byte i = 0; i < INDICATORS_AMOUNT; i++)
+    {
+      if (indicatorsToFlip[i])
+      {
+        if (startCathode[i] > endCathode[i])
+        {
+          startCathode[i]--;
+          indicators.digits[i] = CATHOD_TO_DIGIT[startCathode[i]];
+        }
+        else if (startCathode[i] < endCathode[i])
+        {
+          startCathode[i]++;
+          indicators.digits[i] = CATHOD_TO_DIGIT[startCathode[i]];
+        }
+        else
+        {
+          indicatorsToFlip[i] = false;
+        }
+      }
+      else
+      {
+        flipCounter++;
+      }
+    }
+    if (flipCounter == INDICATORS_AMOUNT)
+    {
+      flipInProgress = false;
+      return false;
+    }
+    return true;
+  }
+
+  bool tickEffectTrain(byte hrs, byte mins, byte newTime[])
+  {
+    if (!flipInProgress)
+    {
+      flipInProgress = true;
+      currentLamp = 0;
+      trainLeaving = true;
+      flipTimer.reset();
+    }
+    if (!flipTimer.isReady())
+    {
+      return true;
+    }
+    if (trainLeaving)
+    {
+      for (byte i = 3; i > currentLamp; i--)
+      {
+        indicators.digits[i] = indicators.digits[i - 1];
+      }
+      indicators.turnOff(currentLamp);
+      currentLamp++;
+      if (currentLamp >= INDICATORS_AMOUNT)
+      {
+        trainLeaving = false;
+        currentLamp = 0;
+      }
+    }
+    else
+    {
+      for (byte i = currentLamp; i > 0; i--)
+      {
+        indicators.digits[i] = indicators.digits[i - 1];
+      }
+      indicators.digits[0] = newTime[3 - currentLamp];
+      indicators.turnOn(currentLamp);
+      currentLamp++;
+      if (currentLamp >= INDICATORS_AMOUNT)
+      {
+        flipInProgress = false;
+        return false;
+      }
+    }
+    return true;
+  }
+
+  bool tickEffectRubber(byte hrs, byte mins, byte newTime[])
+  {
+    if (!flipInProgress)
+    {
+      flipInProgress = true;
+      flipEffectStages = 0;
+      flipTimer.reset();
+    }
+    if (!flipTimer.isReady())
+    {
+      return true;
+    }
+    switch (flipEffectStages++)
+    {
+    case 1:
+      indicators.turnOff(3);
+      break;
+    case 2:
+      indicators.turnOff(2);
+      indicators.digits[3] = indicators.digits[2];
+      indicators.turnOn(3);
+      break;
+    case 3:
+      indicators.turnOff(3);
+      break;
+    case 4:
+      indicators.turnOff(1);
+      indicators.digits[2] = indicators.digits[1];
+      indicators.turnOn(2);
+      break;
+    case 5:
+      indicators.turnOff(2);
+      indicators.digits[3] = indicators.digits[1];
+      indicators.turnOn(3);
+      break;
+    case 6:
+      indicators.turnOff(3);
+      break;
+    case 7:
+      indicators.turnOff(0);
+      indicators.digits[1] = indicators.digits[0];
+      indicators.turnOn(1);
+      break;
+    case 8:
+      indicators.turnOff(1);
+      indicators.digits[2] = indicators.digits[0];
+      indicators.turnOn(2);
+      break;
+    case 9:
+      indicators.turnOff(2);
+      indicators.digits[3] = indicators.digits[0];
+      indicators.turnOn(3);
+      break;
+    case 10:
+      indicators.turnOff(3);
+      break;
+    case 11:
+      indicators.digits[0] = newTime[3];
+      indicators.turnOn(0);
+      break;
+    case 12:
+      indicators.turnOff(0);
+      indicators.digits[1] = newTime[3];
+      indicators.turnOn(1);
+      break;
+    case 13:
+      indicators.turnOff(1);
+      indicators.digits[2] = newTime[3];
+      indicators.turnOn(2);
+      break;
+    case 14:
+      indicators.turnOff(2);
+      indicators.digits[3] = newTime[3];
+      indicators.turnOn(3);
+      break;
+    case 15:
+      indicators.digits[0] = newTime[2];
+      indicators.turnOn(0);
+      break;
+    case 16:
+      indicators.turnOff(0);
+      indicators.digits[1] = newTime[2];
+      indicators.turnOn(1);
+      break;
+    case 17:
+      indicators.turnOff(1);
+      indicators.digits[2] = newTime[2];
+      indicators.turnOn(2);
+      break;
+    case 18:
+      indicators.digits[0] = newTime[1];
+      indicators.turnOn(0);
+      break;
+    case 19:
+      indicators.turnOff(0);
+      indicators.digits[1] = newTime[1];
+      indicators.turnOn(1);
+      break;
+    case 20:
+      indicators.digits[0] = newTime[0];
+      indicators.turnOn(0);
+      break;
+    case 21:
+      flipInProgress = false;
+      return false;
+    }
+    return true;
+  }
 };
 
 Effects effects;
