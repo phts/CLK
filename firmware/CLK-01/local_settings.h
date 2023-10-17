84335adb4234b3e28aaf89a77c36e94a172fb64f
Phil Tsaryk <phil@tsaryk.com>
2023-10-17 23:22:32 +0200

    chore: local settings for this device

---
+++ firmware/CLK-01/local_settings.h
@@ -0,0 +1,13 @@
+#ifndef local_settings_h
+#define local_settings_h
+
+// #define DEBUG true
+
+#define NIGHT_START 21
+#define NIGHT_END 8
+#define STANDBY_WAKEUP_ON_TIME_CHANGE false
+#define STANDBY_WAKEUP_RANDOM false
+#define STANDBY_KEEP_DOT false
+#define STANDBY_TIMEOUT 60
+
+#endif
